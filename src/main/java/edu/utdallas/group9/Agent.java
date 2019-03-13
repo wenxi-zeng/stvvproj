cc774fb14f1f2604cdcf511c57bf69587dc36fd3
song4u <song4you@msn.cn>
2019-03-12 20:47:59 -0500

    java agent draft

--- src/main/java/edu/utdallas/group9/Agent.java
+++ src/main/java/edu/utdallas/group9/Agent.java
@@ -1,9 +1,42 @@
 package edu.utdallas.group9;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 
 public class Agent {
     public static void premain(String agentArgs, Instrumentation inst) {
-        inst.addTransformer(new ClassTransformer());
+        String packageName = getPackgeName();
+        if (packageName == null)
+            System.out.println("Failed to read package name from pom.xml");
+        else
+            inst.addTransformer(new ClassTransformer(packageName));
+    }
+
+    private static String getPackgeName() {
+        try {
+            File fXmlFile = new File("pom.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+            Element node = doc.getElementById("groupId");
+            return node.getTextContent();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
